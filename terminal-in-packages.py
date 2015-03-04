d16e8415dcf0287b71827e0db5902bf32c5abf6f
Manuel <p513manu@gmail.com>
2015-03-04 11:42:36 -0600

    fixes

--- terminal-in-packages.py
+++ terminal-in-packages.py
@@ -7,27 +7,26 @@ import platform
 
 PLATFORM = platform.system()
 TERMINAL = ''
-log = print
 
 
 class CommandThread(threading.Thread):
 
     def __init__(self, command):
         self.command = command
+        self.package_dir = sublime.packages_path()
         threading.Thread.__init__(self)
 
     def run(self):
-        command = "{}".format(self.command)
         env = os.environ.copy()
         if PLATFORM == 'Windows':
             command = [
                 'cmd.exe',
-                '/k', "{} && timeout /T 10 && exit".format(command)
+                '/k', "{} && timeout /T 10 && exit".format(self.command)
             ]
         if PLATFORM == 'Linux':
             command = [
                 TERMINAL,
-                '-e', 'bash -c \"{0}; read line\"'.format(command)
+                '-e', 'bash -c \"{0}; read line\"'.format(self.command)
             ]
         if PLATFORM == 'Darwin':
             command = [
@@ -36,28 +35,23 @@ class CommandThread(threading.Thread):
                 '-e', 'tell application "System Events" to tell process \
                 "Terminal" to keystroke "t" using command down',
                 '-e', 'tell application "Terminal" to \
-                do script "{0}" in front window'.format(command)
+                do script "{0}" in front window'.format(self.command)
             ]
-
-        log('Command is : {0}'.format(str(command)))
-        subprocess.Popen(command, env=env)
+        subprocess.Popen(command, env=env, cwd=self.package_dir)
 
 
 class TerminalInPackagesCommand(sublime_plugin.WindowCommand):
 
     def __init__(self, *args, **kwargs):
         self.settings = sublime.load_settings('terminalinpackages.sublime-settings')
-        global TERMINAL
-        TERMINAL = self.settings.get('terminal-emulator')
         super(TerminalInPackagesCommand, self).__init__(*args, **kwargs)
 
     def run(self):
-        pdir = sublime.packages_path()
-        change_dir_command = 'cd {0}'.format(pdir)
         if PLATFORM == 'Windows':
-            command = 'cmd /k {}'.format(change_dir_command)
+            command = 'cmd'
         if PLATFORM == 'Linux' or PLATFORM == 'Darwin':
-            command = "bash --rcfile <(echo '. ~/.bashrc && {}')".format(change_dir_command)
-        print(command)
+            command = "bash"
+        global TERMINAL
+        TERMINAL = self.settings.get('terminal-emulator')
         thread = CommandThread(command)
         thread.start()
